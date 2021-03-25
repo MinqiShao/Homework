a98cdbc8da8ca9dbe3650509b026357f9ca0a099
MinqiShao <1179731085@qq.com>
2021-03-25 10:01:13 +0800

    Add files via upload

---
+++ homework4/one/Program.cs
@@ -0,0 +1,75 @@
+﻿//为示例中的泛型链表类添加类似于List<T>类的ForEach(Action<T> action)方法。通过调用这个方法打印链表元素，求最大值、最小值和求和（使用lambda表达式实现）
+
+using System;
+
+namespace one
+{
+    public delegate void Func<T>(Node<T> n);
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+                head = tail = n;
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        
+        public void ForEach(Func<T> func)
+        {
+            Node<T> n = this.head;
+            while (n != null)
+            {
+                func(n);
+                n = n.Next;
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list=new GenericList<int>();
+            for(int i = 0; i < 5; i++)
+            {
+                Random random = new Random();
+                list.Add(random.Next(50));
+            }
+            list.ForEach(m => Console.Write(m.Data+" "));
+            int sum = 0;
+            list.ForEach(m => sum += m.Data);
+            Console.WriteLine($"sum={sum}");
+            int max = 0;
+            list.ForEach(m => { if (m.Data > max) max = m.Data; });
+            Console.WriteLine($"max={max}");
+            int min = 123;
+            list.ForEach(m => { if (m.Data <min) min = m.Data; });
+            Console.WriteLine($"min={min}");
+        }
+    }
+}
---
+++ homework4/two/InValidException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace two
+{
+    class InValidException:Exception
+    {
+        public string Code { get; set; }
+        public InValidException(int hour,int minute,int second)
+        {
+            if (hour > 24)
+                Code = "1";
+            if (minute > 60)
+                Code = "2";
+            if(second>60)
+                Code="3";
+        }
+    }
+}
